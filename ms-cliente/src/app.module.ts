9c648c140f9a886217860a24942b392c5ca95cd2
Gustavo Panchi <pgustavo_raul@yahoo.com>
2022-02-24 21:18:35 -0500

    configuración de orm

--- ms-cliente/src/app.module.ts
+++ ms-cliente/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
