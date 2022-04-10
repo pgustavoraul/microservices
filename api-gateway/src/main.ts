03214f655583d49c37d41dd2685d42d91f830bfe
Gustavo Panchi <pgustavo_raul@yahoo.com>
2022-04-10 18:16:44 -0500

    ajuste para registro de logs

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -6,7 +6,9 @@ import * as compression from 'compression';
 import { initSwagger } from './app.swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'debug', 'log'],
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
