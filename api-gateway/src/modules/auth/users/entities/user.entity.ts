a7e38b7a1666591a80d1aad023434bf9b5756d6e
Gustavo Panchi <gustavo.panchi@msp.gob.ec>
2022-04-26 09:17:01 -0500

    ajustes del modelo de datos

--- api-gateway/src/modules/auth/users/entities/user.entity.ts
+++ api-gateway/src/modules/auth/users/entities/user.entity.ts
@@ -1,15 +1,20 @@
 
+import { Field, Int, ObjectType } from '@nestjs/graphql';
 import { BaseEntity } from 'src/modules/shared/entities/base.entity';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@ObjectType()
 @Entity('user')
-export class UserEntity extends BaseEntity{
+export class User extends BaseEntity{
+  @Field(() => Int, { description: 'ID Usuario' })
   @PrimaryGeneratedColumn('increment')
   id: number;
 
+  @Field(() => String, { description: 'NIC Usuario' })
   @Column({ length: 30, nullable: false })
   nic: string;
 
+  @Field(() => String, { description: 'Password Usuario' })
   @Column({ length: 100, nullable: false })
   password: string;
 }
--- api-gateway/src/modules/people/persons/entities/person.entity.ts
+++ api-gateway/src/modules/people/persons/entities/person.entity.ts
@@ -1,23 +1,31 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
 import { BaseEntity } from 'src/modules/shared/entities/base.entity';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@ObjectType()
 @Entity('person')
-export class PersonEntity extends BaseEntity{
+export class Person extends BaseEntity{
+  @Field(() => Int, { description: 'Persona ID' })
   @PrimaryGeneratedColumn('increment')
   id: number;
 
+  @Field(() => String, { description: 'Nombres' })
   @Column({ length: 30, nullable: false })
   firstname: string;
 
+  @Field(() => String, { description: 'Apellidos' })
   @Column({ length: 30, nullable: false })
   lastname: string;
 
+  @Field(() => Date, { description: 'Fecha de Nacimiento' })
   @Column({ type: 'date', nullable: true })
   datebird?: Date;
 
+  @Field(() => Int, { description: 'Teléfono 1' })
   @Column({ length: 15, nullable: false })
   phone1: string;
 
+  @Field(() => Int, { description: 'Teléfono 2' })
   @Column({ length: 15, nullable: true })
   phone2?: string;
 }
